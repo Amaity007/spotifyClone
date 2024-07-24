aac5d3dbd9f4bc813e82c53a85331bfd5ff7cba9
Amaity007 <amaity2468@gmail.com>
2024-07-25 02:47:08 +0530

    cloneOfUiOfSpotifyWebsite

---
+++ spotifyWebScript.js
@@ -0,0 +1,13 @@
+let anchor = document.querySelectorAll(".anchor");
+for (let i = 0; i < anchor.length; i++) {
+    anchor[i].addEventListener("click", () => {
+        alert("This feature is currently unavailable!!!");
+    })
+}
+
+let allButton = document.querySelectorAll(".getBtn");
+for (let i = 0; i < allButton.length; i++) {
+    allButton[i].addEventListener("click", function () {
+        alert("Currently under development phase!!!");
+    })
+}
